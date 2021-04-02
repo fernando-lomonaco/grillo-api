7de04faa5bf32a21824141c64f45d39faa56c1f7
Fernando G. Lomonaco <fernando_lomonaco@outlook.com>
2021-04-02 00:17:57 -0300

    update tests

--- src/test/java/br/com/grillo/controller/CategoryControllerTest.java
+++ src/test/java/br/com/grillo/controller/CategoryControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.grillo.controller;
 
-import br.com.grillo.manager.LifeCycleManager;
 import br.com.grillo.model.CategoryModel;
 import br.com.grillo.model.entity.Category;
 import br.com.grillo.model.resource.CategoryModelAssembler;
@@ -19,31 +18,29 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.text.ParseException;
-import java.util.UUID;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("integration-test")
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ContextConfiguration(initializers = {LifeCycleManager.class})
-public class CategoryControllerTest {
+class CategoryControllerTest {
 
-    private static final String CODE = "05ae9332-394f-4d6a-8823-2245f6d52bce";
-    private static final String NAME = "Bebidas";
-    private static final String DESCRIPTION = "Bebida sem alcool como Refrigerantes e Sucos";
-    private static final String STATUS = "A";
-    private static final String TRANSACTION_DATE = "2020-08-21T18:32:04.150";
-    private static final String URL = "/categories";
+    static final Long CODE = 39L;
+    static final String NAME = "Bebidas";
+    static final String DESCRIPTION = "Bebida sem alcool como Refrigerantes e Sucos";
+    static final String STATUS = "A";
+    static final String TRANSACTION_DATE = "2020-08-21T18:32:04.150";
+    static final String URL = "/categories";
 
     private HttpHeaders headers;
 
@@ -57,7 +54,7 @@ public class CategoryControllerTest {
     CategoryModelAssembler assembler;
 
     @BeforeAll
-    private void setUp() {
+    void setUp() {
         headers = new HttpHeaders();
         headers.set("Authorization", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsb21vbmFjbyIsImlhdCI6MTYwMDk2Mzk0NiwiZXhwIjoxNjAxMDUwMzQ2fQ.MF6xpM8qFLqOb8yPi3nCppFdVMpADU4uZlLvTktJXZQtTQPnt2B_kAwBS8w17Xr6_nysmaxLPOpmrLOlbKaJ-Q");
     }
@@ -65,7 +62,7 @@ public class CategoryControllerTest {
     @Test
     @DisplayName("Teste da acao - salvar categoria")
     @Order(1)
-    public void testSave() throws Exception {
+    void testEndpointSave() throws Exception {
 
         Category category = getMockCategory();
 
@@ -85,13 +82,20 @@ public class CategoryControllerTest {
     @Test
     @DisplayName("Teste da acao - salvar categoria com campo invalido")
     @Order(2)
-    public void testSaveInvalidTransaction() throws Exception {
+    void testEndpointSaveInvalidTransaction() throws Exception {
 
         BDDMockito.given(categoryService.save(Mockito.any(Category.class))).willReturn(getMockCategory());
 
-        Category category = new Category(UUID.fromString(CODE), null, DESCRIPTION, STATUS.charAt(0), DateUtils.getLocalDateTimeFromString
-                (TRANSACTION_DATE.concat("Z")), DateUtils.getLocalDateTimeFromString
-                (TRANSACTION_DATE.concat("Z")));
+        Category category = Category.builder()
+                .code(CODE)
+                .name(null)
+                .description(DESCRIPTION)
+                .status(STATUS.charAt(0))
+                .createdDate(DateUtils.getLocalDateTimeFromString
+                        (TRANSACTION_DATE.concat("Z")))
+                .updatedDate(DateUtils.getLocalDateTimeFromString
+                        (TRANSACTION_DATE.concat("Z")))
+                .build();
 
         mockMvc.perform(post(URL).content(getJsonPayload(category))
                 .contentType(MediaTypes.HAL_JSON)
@@ -100,14 +104,21 @@ public class CategoryControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("Request has invalids fields"));
 
-
     }
 
     private Category getMockCategory() throws ParseException {
 
-        return new Category(UUID.fromString(CODE), NAME, DESCRIPTION, STATUS.charAt(0), DateUtils.getLocalDateTimeFromString
-                (TRANSACTION_DATE.concat("Z")), DateUtils.getLocalDateTimeFromString
-                (TRANSACTION_DATE.concat("Z")));
+        return Category.builder()
+                .code(CODE)
+                .name(NAME)
+                .description(DESCRIPTION)
+                .status(STATUS.charAt(0))
+                .createdDate(DateUtils.getLocalDateTimeFromString
+                        (TRANSACTION_DATE.concat("Z")))
+                .updatedDate(DateUtils.getLocalDateTimeFromString
+                        (TRANSACTION_DATE.concat("Z")))
+                .build();
+
     }
 
     private String getJsonPayload(Category category) throws JsonProcessingException {
