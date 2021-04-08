ed57a79f031955463d54ddee46778409b60bdc59
Fernando G. Lomonaco <fernando_lomonaco@outlook.com>
2021-04-08 00:45:16 -0300

    update properties test ehcache file and init

--- src/main/java/br/com/grillo/GrilloApplication.java
+++ src/main/java/br/com/grillo/GrilloApplication.java
@@ -1,13 +1,18 @@
 package br.com.grillo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @SpringBootApplication
 public class GrilloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrilloApplication.class, args);
+		log.info("GrilloApplication started successfully at {}", LocalDateTime.now());
 	}
 
 }
